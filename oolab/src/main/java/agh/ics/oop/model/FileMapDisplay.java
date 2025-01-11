bc3a595c9f04fea35b49e8f80458ac833ce81dc7
kasiabeben10 <96909263+kasiabeben10@users.noreply.github.com>
2025-01-11 17:24:24 +0100

    update path in FileMapDisplay

--- oolab/src/main/java/agh/ics/oop/model/FileMapDisplay.java
+++ oolab/src/main/java/agh/ics/oop/model/FileMapDisplay.java
@@ -7,12 +7,14 @@ public class FileMapDisplay implements MapChangeListener {
 
     @Override
     public void mapChanged(WorldMap worldMap, String message) {
-        try (FileWriter writer = new FileWriter("map_id.log",true)) {
+
+        String fileName = "./simulation_logs/map_%s.log".formatted(worldMap.getId());
+
+        try (FileWriter writer = new FileWriter(fileName,true)) {
             writer.write(message + System.lineSeparator());
             writer.write(worldMap.toString());
         } catch (IOException e){
-            e.printStackTrace();
-            System.err.println("Failed to write to file: " + e.getMessage());
+            System.out.println(e.getMessage());
         }
     }
 }
